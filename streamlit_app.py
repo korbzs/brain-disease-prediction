de76810b1bc83628100941d1fe2af7816afa5f6b
korbzs <146817924+korbzs@users.noreply.github.com>
2024-11-12 12:13:17 +0100

    global_hog_pca_rf 89%

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,3 @@ st.file_uploader("Please upload photo from your brain CT image", ["png", "jpg", 
 
 st.image("agyikepek_3_osztaly\\kepek\\1732_3.png", caption="Tumorous Brain")
 st.write(f"The brain is tumorous with {Brain_Model.accuracy*100}% certainty")
-
